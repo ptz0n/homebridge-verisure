05f6ceee260c82f2ad817b3571482933e43095b1
Erik Eng <erik@eng.se>
2018-06-03 02:40:48 +0200

    Return humidity service from climate sensor if supported

--- accessories/climatesensor.js
+++ accessories/climatesensor.js
@@ -16,25 +16,23 @@ class ClimateSensor extends VerisureAccessory {
   constructor(...args) {
     super(...args);
 
-    const { deviceArea, deviceType, temperature } = this.config;
+    const { deviceArea, deviceType } = this.config;
     const _ = i18n(this.installation.locale);
     const name = _(deviceNames[deviceType]) || deviceType;
 
     this.model = deviceType;
     this.name = VerisureAccessory.getUniqueAccessoryName(`${name} (${deviceArea})`);
-    this.value = temperature;
   }
 
-  getCurrentTemperature(callback) {
-    this.log('Getting current temperature.');
+  getCurrentPropertyValue(property, callback) {
+    this.log(`Getting current ${property} value.`);
 
     this.installation.getOverview()
       .then(overview =>
         overview.climateValues.find(device =>
           device.deviceLabel === this.serialNumber))
       .then((device) => {
-        this.value = device.temperature;
-        callback(null, this.value);
+        callback(null, device[property]);
       })
       .catch(callback);
   }
@@ -42,14 +40,27 @@ class ClimateSensor extends VerisureAccessory {
   getServices() {
     const { Service, Characteristic } = this.homebridge.hap;
 
-    this.service = new Service.TemperatureSensor(this.name);
-    this.service
-      .getCharacteristic(Characteristic.CurrentTemperature)
-      .on('get', this.getCurrentTemperature.bind(this));
+    const services = [];
 
-    this.accessoryInformation.setCharacteristic(Characteristic.Model, this.model);
+    services.push(this.accessoryInformation.setCharacteristic(Characteristic.Model, this.model));
 
-    return [this.accessoryInformation, this.service];
+    if (this.config.temperature) {
+      this.temperatureService = new Service.TemperatureSensor(this.name);
+      this.temperatureService
+        .getCharacteristic(Characteristic.CurrentTemperature)
+        .on('get', this.getCurrentPropertyValue.bind(this, 'temperature'));
+      services.push(this.temperatureService);
+    }
+
+    if (this.config.humidity) {
+      this.humidityService = new Service.HumiditySensor(this.name);
+      this.humidityService
+        .getCharacteristic(Characteristic.CurrentRelativeHumidity)
+        .on('get', this.getCurrentPropertyValue.bind(this, 'humidity'));
+      services.push(this.humidityService);
+    }
+
+    return services;
   }
 }
 
--- accessories/climatesensor.test.js
+++ accessories/climatesensor.test.js
@@ -10,6 +10,7 @@ describe('ClimateSensor', () => {
     deviceLabel: 'asd123',
     deviceType: 'SMOKE2',
     temperature: 22,
+    humidity: 55,
   };
   const installation = {
     getOverview: null,
@@ -20,24 +21,40 @@ describe('ClimateSensor', () => {
 
   it('setup name and value', () => {
     expect(climateSensor.name).toBe('Rökdetektor (Hallway)');
-    expect(climateSensor.value).toBe(22);
   });
 
   it('gets current temperature', (done) => {
-    expect.assertions(3);
+    expect.assertions(2);
 
     installation.getOverview = jest.fn();
     installation.getOverview.mockResolvedValueOnce({
       climateValues: [{
-        temperature: 22.5,
         deviceLabel: 'asd123',
+        temperature: 22.5,
       }],
     });
 
-    climateSensor.getCurrentTemperature((error, value) => {
+    climateSensor.getCurrentPropertyValue('temperature', (error, value) => {
       expect(error).toBeNull();
       expect(value).toBe(22.5);
-      expect(climateSensor.value).toBe(22.5);
+      done();
+    });
+  });
+
+  it('gets current relative humidity', (done) => {
+    expect.assertions(2);
+
+    installation.getOverview = jest.fn();
+    installation.getOverview.mockResolvedValueOnce({
+      climateValues: [{
+        deviceLabel: 'asd123',
+        humidity: 62,
+      }],
+    });
+
+    climateSensor.getCurrentPropertyValue('humidity', (error, value) => {
+      expect(error).toBeNull();
+      expect(value).toBe(62);
       done();
     });
   });
@@ -49,9 +66,20 @@ describe('ClimateSensor', () => {
     expect(anotherClimateSensor.name).toBe('FOOBAR (Kitchen)');
   });
 
-  it('expose services', () => {
-    const services = climateSensor.getServices();
-    expect(services.length).toBe(2);
+  it('expose only accessory & temp service', () => {
+    const tempConfig = {
+      deviceLabel: 'asd234',
+      temperature: 22,
+    };
+    const tempSensor = new ClimateSensor(homebridge, logger, tempConfig, installation);
+    const services = tempSensor.getServices();
     expect(Array.isArray(services)).toBe(true);
+    expect(services.length).toBe(2);
+  });
+
+  it('expose all services', () => {
+    const services = climateSensor.getServices();
+    expect(Array.isArray(services)).toBe(true);
+    expect(services.length).toBe(3);
   });
 });
